56403032c044e92a3a3c3b54a781ba5a3439d084
Merrick Minogue <yt7i0aoi@students.codeinstitute.net>
2025-01-23 15:22:00 +0000

    Correct typos causing test failure

--- test_student.py
+++ test_student.py
@@ -12,7 +12,7 @@ class TestStudent(unittest.TestCase):
     def test_email(self):
         student = Student('John', 'Doe')
 
-        self.assertEqual(studen.email, 'johndoe@email.com')
+        self.assertEqual(student.email, 'john.doe@email.com')
     
     
     def test_alert_santa(self):
